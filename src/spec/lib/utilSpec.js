153e6d2760cfafdec9fcf722bed155eaf167b969
Rayan Kanso <rayankans@google.com>
2020-01-07 12:40:04 +0000

    Improve shortcut icon selection.
    Unify icon selection logic & fix some types.

--- src/spec/lib/utilSpec.js
+++ src/spec/lib/utilSpec.js
@@ -19,78 +19,71 @@
 const util = require('../../lib/util');
 
 describe('util', () => {
-  describe('findSuitableIcon', () => {
-    it('returns null for an null manifest', () => {
-      const result = util.findSuitableIcon(null, 'any');
-      expect(result).toBeNull();
-    });
+  it('returns null for an empty icon list', () => {
+    const result = util.findSuitableIcon([], 'any');
+    expect(result).toBeNull();
+  });
 
-    it('returns null for an empty manifest', () => {
-      const result = util.findSuitableIcon({}, 'any');
-      expect(result).toBeNull();
-    });
-
-    it('returns null for an empty icon list', () => {
-      const result = util.findSuitableIcon({
-        icons: [],
-      }, 'any');
-      expect(result).toBeNull();
-    });
-
-    it('returns null for a list with invalid icons', () => {
-      const result = util.findSuitableIcon({
-        icons: [{
+  it('returns any icon if no minSize is provided', () => {
+    const result = util.findSuitableIcon(
+        [{
           'src': '/favicons/android-chrome-192x192.png',
           'sizes': '192x192',
           'type': 'image/png',
-        }],
-      }, 'any');
-      expect(result).toBeNull();
-    });
+        }], 'any');
+    expect(result.src).toBe('/favicons/android-chrome-192x192.png');
+    expect(result.sizes).toBe('192x192');
+    expect(result.type).toBe('image/png');
+    expect(result.size).toBe(192);
+  });
 
-    it('returns a icon suitable icon', () => {
-      const result = util.findSuitableIcon({
-        icons: [{
+  it('returns null if an icon larger than minSize is not found', () => {
+    const result = util.findSuitableIcon(
+        [{
+          'src': '/favicons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png',
+        }], 'any', 512);
+    expect(result).toBeNull();
+  });
+
+  it('returns a icon suitable icon', () => {
+    const result = util.findSuitableIcon([{
+      'src': '/favicons/android-chrome-512x512.png',
+      'sizes': '512x512',
+      'type': 'image/png',
+    }], 'any', 512);
+    expect(result.src).toBe('/favicons/android-chrome-512x512.png');
+    expect(result.sizes).toBe('512x512');
+    expect(result.type).toBe('image/png');
+    expect(result.size).toBe(512);
+  });
+
+  it('returns null when no maskable icon is available', () => {
+    const result = util.findSuitableIcon(
+        [{
           'src': '/favicons/android-chrome-512x512.png',
           'sizes': '512x512',
           'type': 'image/png',
-        }],
-      }, 'any');
-      expect(result.src).toBe('/favicons/android-chrome-512x512.png');
-      expect(result.sizes).toBe('512x512');
-      expect(result.type).toBe('image/png');
-      expect(result.size).toBe(512);
-    });
+        }], 'maskable', 512);
+    expect(result).toBeNull();
+  });
 
-    it('returns null when no maskable icon is available', () => {
-      const result = util.findSuitableIcon({
-        icons: [{
-          'src': '/favicons/android-chrome-512x512.png',
-          'sizes': '512x512',
-          'type': 'image/png',
-        }],
-      }, 'maskable');
-      expect(result).toBeNull();
-    });
-
-    it('returns icon when a maskable icon is available', () => {
-      const result = util.findSuitableIcon({
-        icons: [{
-          'src': '/favicons/android-chrome-512x512.png',
-          'sizes': '512x512',
-          'type': 'image/png',
-        }, {
-          'src': '/favicons/icon-maskable-7a2eb399.png',
-          'sizes': '512x512',
-          'type': 'image/png',
-          'purpose': 'maskable',
-        }],
-      }, 'maskable');
-      expect(result.src).toBe('/favicons/icon-maskable-7a2eb399.png');
-      expect(result.sizes).toBe('512x512');
-      expect(result.type).toBe('image/png');
-      expect(result.purpose).toBe('maskable');
-      expect(result.size).toBe(512);
-    });
+  it('returns icon when a maskable icon is available', () => {
+    const result = util.findSuitableIcon([{
+      'src': '/favicons/android-chrome-512x512.png',
+      'sizes': '512x512',
+      'type': 'image/png',
+    }, {
+      'src': '/favicons/icon-maskable-7a2eb399.png',
+      'sizes': '512x512',
+      'type': 'image/png',
+      'purpose': 'maskable',
+    }], 'maskable', 512);
+    expect(result.src).toBe('/favicons/icon-maskable-7a2eb399.png');
+    expect(result.sizes).toBe('512x512');
+    expect(result.type).toBe('image/png');
+    expect(result.purpose).toBe('maskable');
+    expect(result.size).toBe(512);
   });
 });
