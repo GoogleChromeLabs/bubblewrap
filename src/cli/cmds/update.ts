d5944ed7e6825315feadf1c181a7a8cb8e3a1014
Andre Bandarra <andreban@google.com>
2020-02-03 15:35:36 +0000

    `update` upgrades `versionCode` and `versionName`
    - Renamed `appVersion` to `appVersionName` on `TwaManifest`.
    - Added `appVersionCode` to `TwaManifest.
    - Updated the template project to use `appVersionName` and
      `appVersionCode` from the TWA Manifest.
    - Added new option `skipVersinUpgrade` to `update`, to regenerate
      project without updating versions.
    - Added new option `appVersionName` to set a specific version name
      when updating the project
    - `update` generates new versions by increasing `appVersionCode`
      by 1. When generating a new `appVersionName`, the value of
      `--appVersionName` is used if set. Otherwise, an attempt to
      generate it from the `appVersionCode` is made. If not possible,
      the user is prompted to input a version name.

--- src/cli/cmds/update.ts
+++ src/cli/cmds/update.ts
@@ -14,17 +14,79 @@
  *  limitations under the License.
  */
 
+import * as inquirer from 'inquirer';
 import * as path from 'path';
+import Log from '../../lib/Log';
 import {TwaGenerator} from '../../lib/TwaGenerator';
 import {TwaManifest} from '../../lib/TwaManifest';
 import {ParsedArgs} from 'minimist';
 import {APP_NAME} from '../constants';
+import {notEmpty} from '../inputHelpers';
 
+const log = new Log('update');
+
+async function updateVersions(twaManifest: TwaManifest, appVersionNameArg: string): Promise<{
+      appVersionName: string;
+      appVersionCode: number;
+    }> {
+  const previousAppVersionCode = twaManifest.appVersionCode;
+  const appVersionCode = twaManifest.appVersionCode + 1;
+
+  // If a version was passed ar parameter, use it.
+  if (appVersionNameArg) {
+    return {
+      appVersionCode: appVersionCode,
+      appVersionName: appVersionNameArg,
+    };
+  }
+
+  // Otherwise, try to upgrade automatically with the versionCode.
+  if (twaManifest.appVersionName === previousAppVersionCode.toString()) {
+    return {
+      appVersionCode: appVersionCode,
+      appVersionName: appVersionCode.toString(),
+    };
+  }
+
+  // If not not possible, ask the user to input a new version.
+  const result = await inquirer.prompt([{
+    name: 'appVersionName',
+    type: 'input',
+    message: 'versionName for the new App version:',
+    default: twaManifest.appVersionName,
+    validate: notEmpty,
+  }]);
+
+  return {
+    appVersionCode: appVersionCode,
+    appVersionName: result.appVersionName,
+  };
+}
+
+/**
+ * Updates an existing TWA Project using the `twa-manifest.json`.
+ * @param {string} [args.manifest] directory where the command should look for the
+ * `twa-manifest.json`. Defaults to the current folder.
+ * @param {boolean} [args.skipVersionUpgrade] Skips upgrading appVersionCode and appVersionName
+ * if set to true.
+ * @param {string} [args.appVersionName] Value to be used for appVersionName when upgrading
+ * versions. Ignored if `args.skipVersionUpgrade` is set to true.
+ */
 export async function update(args: ParsedArgs): Promise<void> {
   const targetDirectory = args.directory || process.cwd();
   const manifestFile = args.manifest || path.join(process.cwd(), 'twa-manifest.json');
   const twaManifest = await TwaManifest.fromFile(manifestFile);
   twaManifest.generatorApp = APP_NAME;
+
+  if (!args.skipVersionUpgrade) {
+    const newVersionInfo = await updateVersions(twaManifest, args.appVersionName);
+    twaManifest.appVersionName = newVersionInfo.appVersionName;
+    twaManifest.appVersionCode = newVersionInfo.appVersionCode;
+    log.info(`Generated new version with versionName: ${newVersionInfo.appVersionName} and ` +
+        `versionCode: ${newVersionInfo.appVersionCode}`);
+    twaManifest.saveToFile(manifestFile);
+  }
+
   const twaGenerator = new TwaGenerator();
   return await twaGenerator.createTwaProject(targetDirectory, twaManifest);
 }
