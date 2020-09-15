d221556f923ebe2246919c2e684671f04744a367
chenlevy24 <65597962+chenlevy24@users.noreply.github.com>
2020-09-15 14:45:40 +0300

    Config.ts and its tests to work with Prompt + Minor addition to MockPrompt.ts (#336)
    * Changed config.ts and it's tests to work with Prompt + Changed promptConfirm function to be more verstile
    
    * Added some more documantation
    
    * Changed the default value of jdkInstallRequest to be true

--- packages/cli/src/lib/config.ts
+++ packages/cli/src/lib/config.ts
@@ -17,10 +17,10 @@
 
 import {join} from 'path';
 import {homedir} from 'os';
-import {Config, Log, ConsoleLog, JdkInstaller} from '@bubblewrap/core';
-import * as inquirer from 'inquirer';
+import {Config, Log, ConsoleLog, JdkInstaller, JdkHelper, AndroidSdkTools} from '@bubblewrap/core';
 import {existsSync} from 'fs';
 import {promises as fsPromises} from 'fs';
+import {InquirerPrompt, Prompt} from './Prompt';
 
 const DEFAULT_CONFIG_FOLDER = join(homedir(), '.bubblewrap');
 const DEFAULT_CONFIG_NAME = 'config.json';
@@ -30,42 +30,24 @@ const LEGACY_CONFIG_NAME = 'llama-pack-config.json';
 const LEGACY_CONFIG_FILE_PATH = join(LEGACY_CONFIG_FOLDER, LEGACY_CONFIG_NAME);
 const DEFAULT_JDK_FOLDER = join(DEFAULT_CONFIG_FOLDER, 'jdk');
 
-async function createConfig(): Promise<Config> {
-  const installRequest = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'jdk',
-      message: 'Do you want Bubblewrap to install JDK? (Enter "No" to use your JDK installation)',
-      default: true,
-    },
-  ]);
+async function createConfig(prompt: Prompt = new InquirerPrompt()): Promise<Config> {
+  const jdkInstallRequest = await prompt.promptConfirm('Do you want Bubblewrap to install JDK? ' +
+    '(Enter "No" to use your JDK installation)', true);
 
   let jdkPath;
-  if (!installRequest.jdk) {
-    const jdk = await inquirer.prompt([
-      {
-        name: 'path',
-        message: 'Path to your existing JDK:',
-        validate: existsSync,
-      },
-    ]);
-    jdkPath = jdk.path;
+  if (!jdkInstallRequest) {
+    jdkPath = await prompt.promptInput('Path to your existing JDK:', null,
+        JdkHelper.validatePath);
   } else {
     await fsPromises.mkdir(DEFAULT_JDK_FOLDER);
     console.log(`Downloading JDK 8 to ${DEFAULT_JDK_FOLDER}`);
     const jdkInstaller = new JdkInstaller(process);
     jdkPath = await jdkInstaller.install(DEFAULT_JDK_FOLDER);
   }
+  const androidSdkPath = await prompt.promptInput('Path to the Android SDK:', null,
+      AndroidSdkTools.validatePath);
 
-  const androidSdk = await inquirer.prompt([
-    {
-      name: 'path',
-      message: 'Path to the Android SDK:',
-      validate: existsSync,
-    },
-  ]);
-
-  return new Config(jdkPath, androidSdk.path);
+  return new Config(jdkPath, androidSdkPath);
 }
 
 async function renameConfigIfNeeded(log: Log): Promise<void> {
@@ -89,12 +71,12 @@ async function renameConfigIfNeeded(log: Log): Promise<void> {
 }
 
 export async function loadOrCreateConfig(log: Log = new ConsoleLog('config'),
-    path = DEFAULT_CONFIG_FILE_PATH): Promise<Config> {
+    prompt: Prompt = new InquirerPrompt(), path = DEFAULT_CONFIG_FILE_PATH): Promise<Config> {
   await renameConfigIfNeeded(log);
   const existingConfig = await Config.loadConfig(path);
   if (existingConfig) return existingConfig;
 
-  const config = await createConfig();
+  const config = await createConfig(prompt);
   await config.saveConfig(path);
   return config;
 }
