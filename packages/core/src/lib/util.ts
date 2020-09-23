3a85b162391bbda16b85837a59beed6ccf4e2cc7
Chen Levy <chenlevy@google.com>
2020-09-23 07:05:07 +0000

    Removed redundant import

--- packages/core/src/lib/util.ts
+++ packages/core/src/lib/util.ts
@@ -23,7 +23,6 @@ import {exec, execFile, spawn} from 'child_process';
 import {x as extractTar} from 'tar';
 import {WebManifestIcon} from './types/WebManifest';
 import {Log} from './Log';
-import {lookup} from 'mime-types';
 
 const execPromise = promisify(exec);
 const execFilePromise = promisify(execFile);
