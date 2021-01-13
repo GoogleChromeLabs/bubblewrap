a405ab888c3ffeae3c779304d092456824a01a5f
Peter E Conn <conn.peter@gmail.com>
2021-01-13 12:45:45 +0000

    Fix lint issues.

--- packages/cli/src/lib/cmds/init.ts
+++ packages/cli/src/lib/cmds/init.ts
@@ -136,16 +136,16 @@ async function confirmTwaConfig(twaManifest: TwaManifest, prompt: Prompt): Promi
   const playBillingEnabled = await prompt.promptConfirm(messages.promptPlayBilling, false);
   if (playBillingEnabled) {
     twaManifest.alphaDependencies = {
-      enabled: true
-    }
+      enabled: true,
+    };
 
     twaManifest.features = {
       ...twaManifest.features,
       playBilling: {
-        enabled: true
-      }
+        enabled: true,
+      },
     }
-  }
+  };
 
   // Step 5/5 Signing Key Information.
   prompt.printMessage(messages.messageSigningKeyInformation);
