fd04c7004aa44f3e185fd970baac40662dfffc5c
André Cipriani Bandarra <andreban@gmail.com>
2022-04-26 12:02:27 +0100

    Remove unused import

--- packages/cli/src/lib/cmds/build.ts
+++ packages/cli/src/lib/cmds/build.ts
@@ -15,7 +15,7 @@
  */
 
 import {AndroidSdkTools, Config, GradleWrapper, JdkHelper, KeyTool, Log,
-  ConsoleLog, TwaManifest, JarSigner, SigningKeyInfo, Result} from '@bubblewrap/core';
+  ConsoleLog, TwaManifest, JarSigner, SigningKeyInfo} from '@bubblewrap/core';
 import * as fs from 'fs';
 import * as path from 'path';
 import {enUS as messages} from '../strings';
